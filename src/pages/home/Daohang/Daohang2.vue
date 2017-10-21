4b9b3bb87f1ff15fd4baf0f1d0bd41a9968241f2
zhangzitao12 <1505355460@qq.com>
2017-10-21 13:27:20 +0800

    fin

---
+++ src/pages/home/Daohang/Daohang2.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="daohang">
+        <a href=""><img src="../imgs/nav1_2.jpg" alt=""></a>
+   </div>
+</template>
+    
+<script>
+export default {
+  name: "daohang",
+  data () {
+    return {
+        
+    };
+  }
+}
+</script>
+    
+<style lang="css" scoped>
+    #daohang{
+        width: 100%;
+    }
+    /* #daohang div{
+        display: flex;
+        height: 0.675rem;
+    } */
+   
+    #daohang  a{
+        display: block;
+        width: 100%;
+         height: 0.675rem;
+         /* font-size: 0.2rem; */
+    }
+    #daohang  a img{
+       width: 100%;
+       height: 100%;
+    }
+</style>
